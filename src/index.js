248c3246d9a4d293855936fcd848ac89b647c471
Julius Camarillo <julcamarillo17@gmail.com>
2019-03-12 22:08:07 -0500

    Adding recipes, setting up state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {PanelGroup,Panel,Button,ButtonToolbar,ListGroup,ListGroupItem} from 'react-bootstrap';
+//create the main class for displaying the recipes
+
+class Recipe extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			recipes: [
+			{name:"BLT", ingredients: ["2 slices of bread", "2 tomato slices","3 strips of bacon","1 wedge of iceberg lettuce","1 tbsp of mayo"]}
+			]
+		}
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
