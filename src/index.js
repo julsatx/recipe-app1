e34debad7d5dd42d13c8df8e6585d68e55e4dc2d
Julius Camarillo <julcamarillo17@gmail.com>
2019-03-12 22:23:11 -0500

    first view created

--- src/index.js
+++ src/index.js
@@ -11,15 +11,39 @@ class Recipe extends Component {
 		super(props);
 		this.state = {
 			recipes: [
-			{name:"BLT", ingredients: ["2 slices of bread", "2 tomato slices","3 strips of bacon","1 wedge of iceberg lettuce","1 tbsp of mayo"]}
+			{name:"BLT", ingredients: ["2 slices of bread", "2 tomato slices","3 strips of bacon","1 wedge of iceberg lettuce","1 tbsp of mayo"]},{name: "French Toast", ingredients: ["4 slices of Italian bread","4 Eggs","1 cup milk","1/2 tbsp nutmeg","1/2 tbsp cinnamon"]}
 			]
-		}
+		};
 	}
-}
+	render() {
+    const recipes = this.state.recipes;
+    return(
+      <div className="Main">
+        <h1>RECIPE BOX</h1>
+        <PanelGroup accordion id="recipes">
+          {recipes.map((recipe, index) => (
+            <Panel eventKey={index} key={index}>
+              <Panel.Heading>
+                <Panel.Title className="title" toggle>{recipe.name}</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body collapsible>
+                <ListGroup>
+                  {recipe.ingredients.map((ingredient, index) => (
+                    <ListGroupItem key={index}>{ingredient}</ListGroupItem>
+                  ))}
+                </ListGroup>
+                <ButtonToolbar>
+                  <Button bsStyle="warning">Edit</Button>
+                  <Button bsStyle="danger">Delete</Button>
+                </ButtonToolbar>
+              </Panel.Body>
+            </Panel>
+          ))}
+        </PanelGroup>
+        <Button bsStyle="primary">Add Recipe</Button>
+      </div>
+    );
+  }
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Recipe />, document.getElementById('app'));
